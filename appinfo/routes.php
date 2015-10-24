fff96b47d2a1f50395cdcf8743545b6ea18b4b77
Klaus <klaus@jsxc.org>
2015-10-24 13:15:19 +0200

    add setSettings route

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -1,4 +1,7 @@
 <?php
 $this->create('user_hiorg_index', '/')
 	->actionInclude('user_hiorg/index.php');
+
+$this->create('user_hiorg_ajax_setsettings', 'ajax/setSettings.php')
+	->actionInclude('user_hiorg/ajax/setSettings.php');
 ?>
