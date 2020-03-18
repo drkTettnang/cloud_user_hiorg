321ad7c9fa9c8322a8a47245c9b38fc312dfab9a
Klaus Herberth <klaus.herberth@drk-tettnang.de>
2020-03-18 11:59:17 +0100

    migrate from database.xml

---
+++ lib/Migration/Version000500Date20200318105554.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OCA\User_Hiorg\Migration;
+
+use Closure;
+use OCP\DB\ISchemaWrapper;
+use OCP\Migration\SimpleMigrationStep;
+use OCP\Migration\IOutput;
+
+/**
+ * Auto-generated migration step: Please modify to your needs!
+ */
+class Version000500Date20200318105554 extends SimpleMigrationStep {
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 */
+	public function preSchemaChange(IOutput $output, Closure $schemaClosure, array $options) {
+	}
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 * @return null|ISchemaWrapper
+	 */
+	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options) {
+		/** @var ISchemaWrapper $schema */
+		$schema = $schemaClosure();
+
+		if (!$schema->hasTable('user_hiorg_users')) {
+			$table = $schema->createTable('user_hiorg_users');
+			$table->addColumn('uid', 'string', [
+				'notnull' => true,
+			]);
+			$table->addColumn('username', 'string', [
+				'notnull' => true,
+			]);
+			$table->addColumn('password', 'string', [
+				'notnull' => true,
+			]);
+			$table->addColumn('displayname', 'string', [
+				'notnull' => false,
+			]);
+			$table->setPrimaryKey(['uid']);
+		}
+		return $schema;
+	}
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 */
+	public function postSchemaChange(IOutput $output, Closure $schemaClosure, array $options) {
+	}
+}
